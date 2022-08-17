3cdebc9bf3c914909188ea09d7a4f3e78a3987b4
vishal Tiwari <vishalti9639@gmail.com>
2022-08-17 20:53:43 +0530

    Learned on 17Aug22

---
+++ Practice Set/vt_practice.java
@@ -0,0 +1,38 @@
+package practice_set;
+import java.util.Scanner;
+
+public class vt_practice {
+	public static void main (String[] args){
+		//Question 1
+		//int a = 4;
+		//int b = 17;
+		//int c = 6;
+		//int sum = a+b+c;
+		//System.out.println(sum);
+		
+		//Question 2
+		//float sub1 = 45;
+		//float sub2 = 95;
+		//float sub3 = 48;
+		//float cgpa = (sub1 + sub2 + sub3)/30;
+		//System.out.println(cgpa);
+		
+		//Question 3
+		//System.out.println("What is your Name: ");
+		Scanner sc = new Scanner(System.in);
+		//String name= sc.next();
+		//System.out.println("helloo! "+name+ " Have a good day.");
+		
+		//Question 4
+		//System.out.println("Input a number to convert into miles :");
+		//float a = sc.nextFloat();
+		//double b = 0.621371;
+		//double miles = a * b;
+		//System.out.println(miles);
+		
+		//Question 5
+		System.out.println("Enter a number :");
+		System.out.println(sc.hasNextInt());
+		
+	}
+}
