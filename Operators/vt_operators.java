a3ec8321ea2e23db8717b71bd2a771b734037d61
vishal Tiwari <vishalti9639@gmail.com>
2022-08-18 20:11:20 +0530

    Operators 18Aug22

---
+++ Operators/vt_operators.java
@@ -0,0 +1,26 @@
+package vt_operators;
+
+public class vt_operators {
+	public static void main(String[] args) {
+		//1. Arithmetic Operators
+		//int a = 4;
+		//int b = 4+a;
+		//System.out.println(b);
+		
+		//2. Assignment Operators
+		//int a = 4;
+		//int b = 9;
+		//b *= 3;
+		//System.out.println(b);
+		
+		//3. Comparison Operators
+		//System.out.println(64>6);
+		
+		//4. Logical Operators
+		//System.out.println(64>5 && 64>60);
+		
+		//5. Bitwise Operators
+		//System.out.println(2&3);
+		
+	}
+}
